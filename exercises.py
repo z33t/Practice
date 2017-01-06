c7213e54f137ab4112f9d5d3d774a3e52c9b5373
z33t <z3ohaib@gmail.com>
2017-01-06 15:50:14 -0500

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -43,3 +43,23 @@ rotate_left3([5, 11, 9]) → [11, 9, 5]
 rotate_left3([7, 0, 0]) → [0, 0, 7]"""
 def rotate_left3(nums):
   return [nums[1],nums[2],nums[0]]
+
+
+"""Given an array of ints length 3, return a new array with the elements in reverse order, so {1, 2, 3} becomes {3, 2, 1}.
+reverse3([1, 2, 3]) → [3, 2, 1]
+reverse3([5, 11, 9]) → [9, 11, 5]
+reverse3([7, 0, 0]) → [0, 0, 7]"""
+def reverse3(nums):
+  nums.reverse()
+  return nums
+
+"""Given an array of ints length 3, figure out which is larger, the first or last element in the array, and set all the other elements to be that value. Return the changed array.
+max_end3([1, 2, 3]) → [3, 3, 3]
+max_end3([11, 5, 9]) → [11, 11, 11]
+max_end3([2, 11, 3]) → [3, 3, 3]"""
+
+def max_end3(nums):
+  if nums[0] >= nums[2]:
+    return [nums[0],nums[0],nums[0]]
+  if nums[0] < nums[2]:
+   return [nums[2],nums[2],nums[2]]
