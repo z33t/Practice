842a8a004534834ec7b24c51c47891d71480a0e6
z33t <z3ohaib@gmail.com>
2017-01-06 15:54:54 -0500

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -63,3 +63,18 @@ def max_end3(nums):
     return [nums[0],nums[0],nums[0]]
   if nums[0] < nums[2]:
    return [nums[2],nums[2],nums[2]]
+
+
+"""Given an array of ints, return the sum of the first 2 elements in the array. If the array length is less than 2, just sum up the elements that exist, returning 0 if the array is length 0.
+sum2([1, 2, 3]) → 3
+sum2([1, 1]) → 2
+sum2([1, 1, 1, 1]) → 2"""
+def sum2(nums):
+  i = 0
+  sum = 0
+  for k in nums:
+    sum += k
+    i +=1
+    if i == 2:
+      return sum
+  return sum
