5f287710095e7db2f414545fa4a8cc4055911fd3
z33t <z3ohaib@gmail.com>
2017-01-06 15:38:50 -0500

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -26,3 +26,13 @@ def common_end(a, b):
     return True
   else:
     return False
+
+"""Given an array of ints length 3, return the sum of all the elements.
+sum3([1, 2, 3]) → 6
+sum3([5, 11, 2]) → 18
+sum3([7, 0, 0]) → 7"""
+def sum3(nums):
+  sum = 0
+  for i in nums:
+      sum += i
+  return sum
