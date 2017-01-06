78714fd5d8c1e6da2b134f0ff0079918d8f3dd49
z33t <z3ohaib@gmail.com>
2017-01-06 15:44:00 -0500

    Update exercises.py

--- exercises.py
+++ exercises.py
@@ -36,3 +36,10 @@ def sum3(nums):
   for i in nums:
       sum += i
   return sum
+
+"""Given an array of ints length 3, return an array with the elements "rotated left" so {1, 2, 3} yields {2, 3, 1}.
+rotate_left3([1, 2, 3]) → [2, 3, 1]
+rotate_left3([5, 11, 9]) → [11, 9, 5]
+rotate_left3([7, 0, 0]) → [0, 0, 7]"""
+def rotate_left3(nums):
+  return [nums[1],nums[2],nums[0]]
